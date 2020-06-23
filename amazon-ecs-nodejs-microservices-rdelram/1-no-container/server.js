cdb4b8ba7db4f4c8588ec18a8364e5b3f9e8c050
amazonAWS-tutorial <67293996+amazonAWS-tutorial@users.noreply.github.com>
2020-06-23 11:17:03 +0200

    first commit

---
+++ amazon-ecs-nodejs-microservices-rdelram/1-no-container/server.js
@@ -0,0 +1,52 @@
+const app = require('koa')();
+const router = require('koa-router')();
+const db = require('./db.json');
+
+// Log requests
+app.use(function *(next){
+  const start = new Date;
+  yield next;
+  const ms = new Date - start;
+  console.log('%s %s - %s', this.method, this.url, ms);
+});
+
+router.get('/rdelram/users-rdelram', function *(next) {
+  this.body = db.users;
+});
+
+router.get('/rdelram/users-rdelram/:userId', function *(next) {
+  const id = parseInt(this.params.userId);
+  this.body = db.users.find((user) => user.id == id);
+});
+
+router.get('/rdelram/threads-rdelram', function *() {
+  this.body = db.threads;
+});
+
+router.get('/rdelram/threads-rdelram/:threadId', function *() {
+  const id = parseInt(this.params.threadId);
+  this.body = db.threads.find((thread) => thread.id == id);
+});
+
+router.get('/rdelram/posts-rdelram/in-thread/:threadId', function *() {
+  const id = parseInt(this.params.threadId);
+  this.body = db.posts.filter((post) => post.thread == id);
+});
+
+router.get('/rdelram/posts-rdelram/by-user/:userId', function *() {
+  const id = parseInt(this.params.userId);
+  this.body = db.posts.filter((post) => post.user == id);
+});
+
+router.get('/rdelram/', function *() {
+  this.body = "API ready to receive requests";
+});
+
+router.get('/', function *() {
+  this.body = "Ready to receive requests";
+});
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+app.listen(3000);
